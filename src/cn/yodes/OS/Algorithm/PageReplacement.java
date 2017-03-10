23ac67ac5bb316b1b544a83e56e596611a2e782e
Yodes Yang <yodeser@gmail.com>
2017-03-11 01:52:01 +0800

    update system operate step

---
+++ src/cn/yodes/OS/Algorithm/PageReplacement.java
@@ -0,0 +1,234 @@
+package cn.yodes.OS.Algorithm;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class PageReplacement {
+	Scanner sc = new Scanner(new BufferedInputStream(System.in));
+	
+	public static void main(String[] args) {
+		new PageReplacement().display();
+	}
+	
+	public void display(){
+		PageReplacement test = new PageReplacement();
+		int sum_page = 20;
+		boolean show;
+		int []page = new int[sum_page];
+		
+		int sum_block = 5;
+		int[] block = new int[sum_block];
+		
+		System.out.println("请选择是否展示所有的演示过程：");
+		System.out.println("1.Yes\t\t\t2.No");
+		if(sc.nextInt() == 1){
+			show = true;
+		}else{
+			show = false;
+		}
+		System.out.print("[内存块数]: " + sum_block + "\n");
+		System.out.print("[页码集合]: \n");
+		for(int i = 0; i < page.length; i++){
+			page[i] = (int) (Math.random()*10);
+			System.out.print(page[i] + "  ");
+		}
+		System.out.println("\n");
+		System.out.println("******************************************************************");
+		test.FIFO(page, block, show);
+		test.LRU(page, block, show);
+		test.OPT(page, block , show);
+	}
+	
+	public void FIFO(int []page , int []block , boolean show){
+		int index_old = 0 , pre = 0 , current = -1 , temp_c = -2;
+		double time = 0;
+		double ratio ; 
+		boolean flag = false;
+		
+		System.out.println("[------------------------------- FIFO算法演示现场 -------------------------------]\n");
+		
+		for(int i = 0; i < block.length; i++){
+			block[i] = -1;
+		}
+		for(int i = 0; i < page.length; i++){
+			flag    = false;
+			temp_c = current;
+			current = -1;
+			for(int j : block){
+				if(page[i] == j)	{flag = true;	break;}
+			}
+			if(!flag){
+				if(pre < block.length){
+					current = pre++;
+				}else{
+					current = index_old++ % block.length;
+				}
+				block[current] = page[i];
+			}
+//			for(int k = 0; k < block.length; k++){
+//				if(k == current){
+//					System.err.print("[" + block[k] + "]\t");
+//				}else{
+//					System.out.print("[" + block[k] + "]\t");
+//				}
+//			}
+			
+			if(temp_c != current){
+				time++ ;
+			}
+			
+			if(show){
+				System.out.print("-回合" + i + "-\t\t\t");
+				for(int k : block){
+					if(k == -1)	{
+						System.out.print("[ ]\t");
+					}else{
+						System.out.print("[" + k + "]\t");
+					}
+				}
+				if(temp_c != current){
+					System.out.print("+");
+				}
+				
+				System.out.println();
+			}
+		}
+		ratio = new Double(( (page.length - time) / page.length) * 100);
+		System.out.printf("FIFO算法的访问命中率为：    \t %.2f " , ratio);
+		System.out.println("%\n");
+	}
+	
+	public void LRU(int[] page , int[] block , boolean show){
+		 int[] temp = new int[block.length];
+		 int temp_c = -2;
+		 double time = 0 , ratio;
+		 int current = 0;
+		 boolean flag = false;
+		 
+		 System.out.println("[------------------------------- LRU算法演示现场 -------------------------------]\n");
+		 
+		 for(int i = 0; i < temp.length; i++){
+			 temp[i] = i;
+			 block[i] = -1;
+		 }
+		 
+		 for(int i = 0; i < page.length; i++){
+			 flag = false;
+			 temp_c = current;
+			 for(int j = 0; j < block.length; j++){
+				 if(page[i] == block[j]){ flag = true;  current = j; 	break;}
+			 }
+			 
+			 if(!flag){
+				 for(int j = 0; j < temp.length; j++){
+					 if( temp[j] == 0 )	{	current = j; 	break;}
+				 }
+				 block[current] = page[i];
+			 
+				 for(int j = 0; j < temp.length; j++){
+					 if(temp[j] > temp[current])	{ temp[j] = (temp[j] - 1) % 4;}
+				 }
+				 temp[current] = temp.length - 1;
+			 }
+			 
+			 if(temp_c != current){
+					time++ ;
+				}
+				if(show){
+					System.out.print("-回合" + i + "-\t\t\t");
+					for(int k : block){
+						if(k == -1)	{
+							System.out.print("[ ]\t");
+						}else{
+							System.out.print("[" + k + "]\t");
+						}
+					}
+					if(temp_c != current){
+						System.out.print("+");
+					}
+					
+					System.out.println();
+				}
+			}
+			ratio = new Double(( (page.length - time) / page.length) * 100);
+			System.out.printf("LRU算法的访问命中率为：     \t %.2f " , ratio);
+			System.out.println("%\n");
+	 }
+	
+	public void OPT(int[] page , int[] block , boolean show){
+		int[] temp = new int[block.length];
+		int temp_c = -2;
+		double time = 0, ratio = 0;
+		int current = 0;
+		boolean flag = false;
+		
+		System.out.println("[------------------------------- OPT算法演示现场 -------------------------------]\n");
+		
+		for(int i = 0; i < block.length; i++){
+			block[i] = -1;
+		}
+		for(int i = 0; i < page.length; i++){
+			flag = false;
+			for(int j : block){	
+				if(j == page[i])	{	flag = true;			}
+			}
+			temp_c = current;
+			if(!flag){
+				if(current < block.length){
+					block[current++] = page[i];
+				}else{
+					temp = new int[block.length];
+					for(int k = 0; k < temp.length; k++)
+						for(int j = i; j < page.length ; j++)
+							if(page[j] != block[k])	{	
+								temp[k]++;
+							} else{
+								break;
+							}
+					current = getIndexOfMaxVal(temp);
+//					System.out.print("m:");
+//					for(int m : temp){
+//						System.out.print(m + "\t");
+//					}
+					block[current] = page[i];
+				}
+			}
+			if(temp_c != current){
+				time++ ;
+			}
+			
+			if(show){
+				System.out.print("-回合" + i + "-\t\t\t");
+				for(int k : block){
+					if(k == -1)	{
+						System.out.print("[ ]\t");
+					}else{
+						System.out.print("[" + k + "]\t");
+					}
+				}
+				if(temp_c != current){
+					System.out.print("+");
+				}
+				
+				System.out.println();
+			}
+		}
+		ratio = new Double(( (page.length - time) / page.length) * 100);
+		System.out.printf("Optimal算法的访问命中率为：\t %.2f " , ratio);
+		System.out.println("%\n");
+	}
+	
+	public int getIndexOfMaxVal(int[] data){
+		int maxVal = data[0];
+		int maxInd = 0;
+		for(int i = 1; i < data.length; i++){
+			if(data[i] > maxVal){
+				maxVal = data[i];
+				maxInd = i;
+			}
+		}
+//		System.out.println("maxInd=" + maxInd);
+		return maxInd;
+	}
+	
+}
--- src/cn/yodes/OS/Main/Main.java
+++ src/cn/yodes/OS/Main/Main.java
@@ -4,42 +4,76 @@ import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
-import cn.yodes.OS.Algorithm.*;
+import cn.yodes.OS.Algorithm.PageReplacement;
 
 public class Main {
 	Scanner sc = new Scanner(new BufferedInputStream(System.in));
 	int no_algorithm = -1;
 	int sum_block	 =  0;
 	int[] block		  	 ;
-	int[] array_page = null;
+	int[] array_page;
 	
 	public void getData(){
+		System.out.println("*********************页面置换算法演示系统*********************");
 		System.out.println("【1】请选择您所要演示的页面置换算法：");
 		System.out.println("\t 1.FIFO（先入先出） \t  2.LRU（最久未使用）  \t 3.OPT（最佳置换）");
+		sc.reset();
+		
 		this.no_algorithm = sc.nextInt();
 		System.out.println("\n【2】请输入内存块总数：");
 		this.sum_block	 = sc.nextInt();
 		System.out.println("\n【3】请输入所有的页码：");
-		System.out.println(sc.nextLine());
-//		initIntArray( sc.nextLine().split(" ") );
+		sc = new Scanner(new BufferedInputStream(System.in));
+		initIntArray( sc.nextLine().split(" ") );
+		System.out.println("***********************开始演示*****************************");
+		System.out.print("[算法]: " + getAlgorithmName(no_algorithm) + "\t\t");
+		System.out.print("[内存块数]: " + sum_block + "\n");
+		System.out.print("[页码集合]: \n");
+		printArray(array_page);
+		System.out.println();
 	}
 	
 	public void initIntArray(String[] arr_s){
 		int i = 0;
+		array_page = new int[arr_s.length];
 		for(String s : arr_s){
 			this.array_page[i++] = Integer.parseInt(s);
 		}
 		this.block = new int[sum_block];
 	}
 	
+	public void printArray(int[] data){
+		for(int i : data){
+			System.out.print(i + "\t");
+		}
+		System.out.println();
+	}
+	
+	public String getAlgorithmName(int number){
+		switch(number){
+			case 1 :  return "FIFO(先入先出)";
+			case 2 :  return "LRU(最久未使用)";
+			case 3 :  return "OPT(最佳置换)";
+			default:  return "未知算法，输入有误";
+		}
+	}
 	public void display(){
-		getData();
-		switch(no_algorithm){
-			case 1 :	new FIFO().start(array_page, block);	break;
-			case 2 :	new LRU().start(array_page, block);		break;
-			case 3 :	new Optimal().start(array_page, block); break;
-			case -1:	System.out.println("Sorry,You must choose one of these algorithms");
-			default:	System.out.println("\t Thanks Your Using!\t");
+		PageReplacement pr = new PageReplacement();
+		System.out.println("请选择：");
+		System.out.println("1.自动演示\t\t\t2.手动演示");
+		
+		if(sc.nextInt() == 1){
+			pr.display();
+		}else{
+			getData();
+			
+			switch(no_algorithm){
+				case 1 :	pr.FIFO(array_page, block , true);		break;
+				case 2 :	pr.LRU(array_page, block , true);		break;
+				case 3 :	pr.OPT(array_page, block , true); 		break;
+				case -1:	System.out.println("Sorry,You must choose one of these algorithms");
+				default:	System.out.println("\t Thanks Your Using!\t");
+			}
 		}
 	}
 	public static void main(String[] args) throws IOException {
